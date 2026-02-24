298c45050ad8e9529984ea30af022a817432a438
KingBobSkibidi <lucianbai0808@gmail.com>
2026-02-23 21:25:50 -0500

    Feb 23: PID tweak
    - turned down D to fix tilt left when driving forward

--- src/autons.cpp
+++ src/autons.cpp
@@ -17,7 +17,7 @@ const int SWING_SPEED = 65;
 ///
 void default_constants() {
   // P, I, D, and Start I
-  chassis.pid_drive_constants_set(20.0, 0.0, 100.0);         // Fwd/rev constants, used for odom and non odom motions
+  chassis.pid_drive_constants_set(20.0, 0.0, 25.0);         // Fwd/rev constants, used for odom and non odom motions
   chassis.pid_heading_constants_set(8.0, 0.0, 35.0);         // IMU heading hold during drives
   chassis.pid_turn_constants_set(3.0, 0.05, 20.0, 15.0);     // Turn in place constants
   chassis.pid_swing_constants_set(6.0, 0.0, 65.0);           // Swing constants
